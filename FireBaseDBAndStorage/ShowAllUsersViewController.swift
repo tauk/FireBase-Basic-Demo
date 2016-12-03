09ce97de692ff953d970077c46ba66ae43387410
tauk <taukhyd@gmail.com>
2016-12-03 22:30:24 +0400

    slightly better displays using subviews

--- FireBaseDBAndStorage/ShowAllUsersViewController.swift
+++ FireBaseDBAndStorage/ShowAllUsersViewController.swift
@@ -31,10 +31,11 @@ class ShowAllUsersViewController: UIViewController {
         //reference to users in database
         self.usersReference = dbReference.child("users")//.queryOrdered(byChild: "username")
         
-        var data = "";
-        
-        var xVal = 50
+        let xVal = 20
         var yVal = 70
+        
+        let imageXVal = 240;
+        var imageYVal = 70;
         
         //data comes into a snapshot object
         self.usersReference.observe(.childAdded, with: { (snapshot) in
@@ -48,6 +49,12 @@ class ShowAllUsersViewController: UIViewController {
             //use the keys to get the values
             let userName = snapshotValue["username"]
             let email = snapshotValue["email"]
+            
+            var nameEmailLabel : UILabel
+            nameEmailLabel = UILabel(frame: CGRect(x:xVal, y:yVal, width:200, height:120))
+            nameEmailLabel.text = "\(userName!)  \(email!)"
+            self.view.addSubview(nameEmailLabel)
+            
             //if there is an imageurl then show the image
             if let imageurl = snapshotValue["imageurl"] {
                 // Points to the root reference
@@ -60,24 +67,29 @@ class ShowAllUsersViewController: UIViewController {
                     if (error != nil) {
                         print("Error downloading images!")
                     } else {
-                        // Data for "images/island.jpg" is returned
+                        
                         let downloadedImage: UIImage! = UIImage(data: data!)
                         
+                        let compressedImageData = UIImageJPEGRepresentation(downloadedImage, 0.2)
+                        
+                        let compressedImage = UIImage(data : compressedImageData!)
                         //add a image view dynamically into the view
                         var imageView : UIImageView
-                        imageView  = UIImageView(frame:CGRect(x:xVal, y:yVal, width:100, height:120));
-                        imageView.image = downloadedImage
+                        imageView  = UIImageView(frame:CGRect(x:imageXVal, y:imageYVal, width:100, height:100));
+                        imageView.image = compressedImage
                         self.view.addSubview(imageView)
                         
-                        xVal = xVal + 30
-                        yVal = yVal + 20
+                        imageYVal = imageYVal + 120
                     }
                 }
                 
             }
             
-            data.append("\n \(userName!) \(email!)")
-            self.usersDataLabel.text = data
+            //xVal = xVal + 30
+            yVal = yVal + 100
+            
+            //data.append("\n \(userName!) \(email!)")
+            //self.usersDataLabel.text = data
             
         })
         
